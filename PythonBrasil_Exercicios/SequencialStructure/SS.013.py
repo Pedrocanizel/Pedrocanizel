0917acc22406b2aa895c4770860e76a1c35c2c52
Pedro Canizela <pedrocanizela@gmail.com>
2022-03-28 16:41:45 -0300

    Calculating ideal weight for man and woman.

---
+++ PythonBrasil_Exercicios/SequencialStructure/SS.013.py
@@ -0,0 +1,6 @@
+height = float(input('Enter your height'))
+iwm = (72.7 * height) - 58
+iww = (62.1 * height) - 44.7
+
+print(f'By your height, your ideal weight is {iwm} if you are a man')
+print(f'By your height, your ideal weight is {iww} if you are a woman')
