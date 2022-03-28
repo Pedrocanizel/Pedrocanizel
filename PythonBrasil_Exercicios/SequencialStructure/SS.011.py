350049719c2fa44bb4f979e6f1b07e46b1435dcb
Pedro Canizela <pedrocanizela@gmail.com>
2022-03-28 16:29:41 -0300

    Mathematical calculations.

---
+++ PythonBrasil_Exercicios/SequencialStructure/SS.011.py
@@ -0,0 +1,12 @@
+n1 = int(input('Enter an int number'))
+n2 = int(input('Enter another int number'))
+n3 = float(input('Enter a real number'))
+
+a = (n1 * 2) * (n2 / 2)
+b = n1 * 3 + n3
+c = n3 ** 3
+
+print(f'The double of the first multiplied by the half of the second is {a}',)
+print(f'The sum of the triple of the first with the third is {b}')
+print(f'The third cubed is {c}')
+
