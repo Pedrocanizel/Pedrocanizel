e746b4069ff68101df79aa4467cc04447ad5d071
Pedro Canizela <pedrocanizela@gmail.com>
2022-03-28 15:41:53 -0300

    The average of 4 notes.

---
+++ PythonBrasil_Exercicios/SequencialStructure/SS.004.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digit the note 1'))
+n2 = float(input('Digit the note 2'))
+n3 = float(input('Digit the note 3'))
+n4 = float(input('Digit the note 4'))
+
+print(f'The average is: {(n1 + n2 + n3 + n4) / 4}')
