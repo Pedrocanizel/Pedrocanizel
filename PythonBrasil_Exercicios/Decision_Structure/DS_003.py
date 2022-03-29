8edbc38e3f4155cff1d43ec04411ab96a04bfccd
Pedro Canizela <pedrocanizela@gmail.com>
2022-03-29 10:19:07 -0300

    Recognizing gender.

---
+++ PythonBrasil_Exercicios/Decision_Structure/DS_003.py
@@ -0,0 +1,10 @@
+gender = input('Enter F or M')
+
+if gender.upper() == 'M':
+    print('The gender is Male')
+
+elif gender.upper() == 'F':
+    print('The gender is Female')
+
+else:
+    print('invalid gender')
