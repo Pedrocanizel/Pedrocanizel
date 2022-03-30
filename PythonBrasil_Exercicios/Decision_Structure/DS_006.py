0d9e80625eb3417bb8894841cfecc4238f343513
Pedro Canizela <pedrocanizela@gmail.com>
2022-03-30 08:30:00 -0300

    The bigger from a list.

---
+++ PythonBrasil_Exercicios/Decision_Structure/DS_006.py
@@ -0,0 +1,8 @@
+ll = []
+
+n1 = int(input('Enter a number'))
+n2 = int(input('Enter a number'))
+n3 = int(input('Enter a number'))
+ll.extend([n1, n2, n3])
+
+print(f'The bigger number is {max(ll)}')
