af0a689f5a14829ed09fc9fd1bd0d844945931c4
Pedro Canizela <pedrocanizela@gmail.com>
2022-05-18 21:57:50 -0300

    Projeto Bus.

--- Busao/Bus2.py
+++ Busao/Bus2.py
@@ -19,33 +19,34 @@ def reservar_assento(x, y):
 
         elif bus[x][y] == 'LIVRE':
             bus[x][y] = 'OCUPADO'
-            print('Seu lugar foi reservado com sucesso')
+            print('Seu lugar foi reservado com sucesso!')
             print(f'O lugar que você escolheu foi marcado como {bus[x][y]}')
 
         elif bus[x][y] == 'OCUPADO':
             print('Assento ocupado, escolha outro!')
 
     except IndexError:
-        print('Os valores que você digitou nao representam uma célula da matriz')
+        print('Os valores que você digitou nao representam uma célula da matriz, '
+              'por favor tente novamente!')
 
 
 while True:
     try:
         x = int(input('Para selecionar seu assento você deve primeiro digitar a linha da célula que deseja reservar,'
-                      'considere a primeira linha como 0 e a última como 7'))
+                      'considere a primeira linha como 0 e a última como 7:'))
         if 0 <= x <= 7:
             break
         else:
             print('Seu número foi menor do que 0 ou maior do que 7, insira um número entre 0 e 7'
-                  'referente á linha na qual se encontra o lugar que você deseja ocupar na matriz')
+                  'referente á linha na qual se encontra o lugar que você deseja ocupar na matriz.')
 
     except ValueError:
-        print('Você digitou caracteres inválidos, insira um número entre 0 e 7')
+        print('Você digitou caracteres inválidos, insira um número entre 0 e 7.')
 
 while True:
     try:
 
-        y = int(input('Agora digite a coluna onde se localiza a célula que deseja reservar'))
+        y = int(input('Agora digite a coluna onde se localiza a célula que deseja reservar:'))
         if 0 <= y <= 4:
             break
         else:
