34f482ffc52bab7c7f6b027d02bd59e77c245238
Pedro Canizela <pedrocanizela@gmail.com>
2022-05-24 13:33:30 -0300

    projeto lab 2

---
+++ Python_Lab_02/lab_02_001.py
@@ -0,0 +1,40 @@
+import datetime
+
+
+class Usuario:
+    def __init__(self, idusu=int, nome=str, sobrenome=str, email=str, bairro=str, nascimento=datetime):
+        self.idusu = idusu
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.email = email
+        self.bairro = bairro
+        self.nascimento = nascimento
+
+
+class Cartao:
+    def __init__(self, idcartao=int, idusu=int, creditos=float, tipocartao=str, emissao=datetime):
+        self.idcartao = idcartao
+        self.idusu = idusu
+        self.creditos = creditos
+        self.tipocartao = ['comum', 'estudante', 'vale-transporte', 'idoso']
+        self.emissao = emissao
+
+
+class Onibus:
+    def __init__(self, numplaca=int, numlinha=int, modelobus=str, anofabrica=datetime, idmotorista=int):
+        self.numplaca = numplaca
+        self.numlinha = numlinha
+        self.modelobus = modelobus
+        self.anofabrica = anofabrica
+        self.idmotorista = idmotorista
+
+
+class Motorista:
+    def __init__(self, idmotorista=int, numcnh=int, nome=str, sobrenome=str, nascimento=datetime):
+        self.idmotorista = idmotorista
+        self.numcnh = numcnh
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.nascimento = nascimento
+
+
