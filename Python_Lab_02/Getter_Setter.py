b37e3c8c555b50a9554bcb948d90950be305d1ef
Pedro Canizela <pedrocanizela@gmail.com>
2022-05-31 14:13:49 -0300

    Getter e setter

--- Python_Lab_02/Getter_Setter.py
+++ Python_Lab_02/Getter_Setter.py
@@ -6,51 +6,57 @@ class Usuario:
         self.bairro = bairro
         self.nascimento = nascimento
 
-    def get_nome(self): #getter
-        return self.nome
+    def get_usuario(self):
+        return self.nome, self.sobrenome, self.email, self.bairro, self.nascimento
 
-    def get_sobrenome(self):
-        return self.sobrenome
+    def set_usuario(self, novo_nome, novo_sobrenome, novo_email, novo_bairro, nova_data):
 
-    def get_email(self):
-        return self.email
-
-    def get_bairro(self):
-        return self.bairro
-
-    def get_nascimento(self):
-        return self.nascimento
-
-    def set_nome(self, novo_nome):#setter
         self.nome = novo_nome
-
-    def set_sobrenome(self, novo_sobrenome):
         self.sobrenome = novo_sobrenome
-
-    def set_email(self, novo_email):
         self.email = novo_email
-
-    def set_bairro(self, novo_bairro):
         self.bairro = novo_bairro
-
-    def set_nascimento(self, nova_data):
         self.nascimento = nova_data
+
+
 class Cartao:
+
     def __init__(self, idcartao=int, idusu=int, creditos=float, tipocartao=str, emissao='emissao'):
+
         self.idcartao = idcartao
         self.idusu = idusu
         self.creditos = creditos
         self.tipocartao = ['comum', 'estudante', 'vale-transporte', 'idoso']
         self.emissao = emissao
 
+    def get_cartao(self):
+        return self.idcartao, self.idusu, self.creditos, self.tipocartao, self.emissao
+
+    def set_cartao(self, novo_id_cartao, novo_idusu, novo_credito, novo_tipo, nova_emissao):
+
+        self.idcartao = novo_id_cartao
+        self.idusu = novo_idusu
+        self.creditos = novo_credito
+        self.tipocartao = novo_tipo
+        self.emissao = nova_emissao
+
 
 class Onibus:
     def __init__(self, numplaca=int, numlinha=int, modelobus=str, anofabrica='ano_fabrica'):
+
         self.numplaca = numplaca
         self.numlinha = numlinha
         self.modelobus = modelobus
         self.anofabrica = anofabrica
 
+    def get_onibus(self):
+        return self.numplaca, self.numlinha, self.modelobus, self.anofabrica
+
+    def set_onibus(self, nova_placa, nova_linha, novo_modelo, nova_anofrabrica):
+
+        self.numplaca = nova_placa
+        self.numlinha = nova_linha
+        self.modelobus = novo_modelo
+        self.anofabrica = nova_anofrabrica
 
 
 class Motorista:
@@ -59,4 +65,14 @@ class Motorista:
         self.nome = nome
         self.sobrenome = sobrenome
         self.nascimento = nascimento
+
+    def get_motorista(self):
+        return self.numcnh, self.nome, self.sobrenome, self.nascimento
+
+    def set_motorista(self, nova_cnh, novo_nome, novo_sobrenome, nova_data):
+
+        self.numcnh = nova_cnh
+        self.nome = novo_nome
+        self.sobrenome = novo_sobrenome
+        self.nascimento = nova_data
 
