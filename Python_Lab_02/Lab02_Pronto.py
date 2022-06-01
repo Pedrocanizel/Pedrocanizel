5124133acb9d3b93847cd28b39157ccaf42f6a33
Pedro Canizela <pedrocanizela@gmail.com>
2022-06-01 15:39:15 -0300

    Atualizado.

--- Python_Lab_02/Lab02_Pronto.py
+++ Python_Lab_02/Lab02_Pronto.py
@@ -1,117 +1,5 @@
-import pyodbc
-import pandas as pd
+from Classes_lab_02 import *
 from os import system
-
-
-server = 'sql-estudo.database.windows.net'
-driver = '{ODBC Driver 17 for SQL Server}'
-database = 'db-estudos'
-username = 'pedro.canizela@blueshift.com.br'
-Authentication = 'ActiveDirectoryInteractive'
-port = '1433'
-
-conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';AUTHENTICATION='+Authentication+';PORT='+port+';DATABASE='+database+';UID='+username)#+';PWD='+password)
-
-
-cursor = conn.cursor()
-
-
-class Usuario:
-    def __init__(self, nome=str, sobrenome=str, email=str, bairro=str, nascimento='nascimento'):
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.email = email
-        self.bairro = bairro
-        self.nascimento = nascimento
-
-    def get_usuario(self):
-        return self.nome, self.sobrenome, self.email, self.bairro, self.nascimento
-
-    def set_usuario(self, novo_nome, novo_sobrenome, novo_email, novo_bairro, nova_data):
-
-        self.nome = novo_nome
-        self.sobrenome = novo_sobrenome
-        self.email = novo_email
-        self.bairro = novo_bairro
-        self.nascimento = nova_data
-        novo_usuario = f"INSERT INTO [PEDRO_CANIZELA_USUARIO] VALUES ('{self.nome}', '{self.sobrenome}', \
-                       '{self.email}','{self.bairro}', '{self.nascimento}' "
-        cursor.execute(novo_usuario)
-        cursor.comit()
-
-
-class Cartao:
-
-    def __init__(self, idcartao=int, idusu=int, creditos=float, tipocartao=str, emissao='emissao'):
-
-        self.idcartao = idcartao
-        self.idusu = idusu
-        self.creditos = creditos
-        self.tipocartao = ['comum', 'estudante', 'vale-transporte', 'idoso']
-        self.emissao = emissao
-
-    def get_cartao(self):
-        return self.idcartao, self.idusu, self.creditos, self.tipocartao, self.emissao
-
-    def set_cartao(self, novo_id_cartao, novo_idusu, novo_credito, novo_tipo, nova_emissao):
-
-        self.idcartao = novo_id_cartao
-        self.idusu = novo_idusu
-        self.creditos = novo_credito
-        self.tipocartao = novo_tipo
-        self.emissao = nova_emissao
-        novo_cartao = f"INSERT INTO [PEDRO_CANIZELA_CARTAO] VALUES ('{self.idcartao}', '{self.idusu}', \
-                      '{self.creditos}','{self.tipocartao}', '{self.emissao}' "
-        cursor.execute(novo_cartao)
-        cursor.comit()
-
-
-class Onibus:
-    def __init__(self, numplaca=int, numlinha=int, modelobus=str, anofabrica='ano_fabrica', id_motorista='id_motorist'):
-
-        self.numplaca = numplaca
-        self.numlinha = numlinha
-        self.modelobus = modelobus
-        self.anofabrica = anofabrica
-        self.id_motorista = id_motorista
-
-    def get_onibus(self):
-        return self.numplaca, self.numlinha, self.modelobus, self.anofabrica
-
-    def set_onibus(self, nova_placa, nova_linha, novo_modelo, nova_anofrabrica, novo_id_motorista):
-
-        self.numplaca = nova_placa
-        self.numlinha = nova_linha
-        self.modelobus = novo_modelo
-        self.anofabrica = nova_anofrabrica
-        self.id_motorista = novo_id_motorista
-        novo_bus = f"INSERT INTO [PEDRO_CANIZELA_ONIBUS] VALUES ('{self.numplaca}', '{self.numlinha}', \
-                   '{self.modelobus}','{self.anofabrica}', '{self.id_motorista}' "
-        cursor.execute(novo_bus)
-        cursor.comit()
-
-
-class Motorista:
-    def __init__(self, numcnh=int, nome=str, sobrenome=str, nascimento='nascimento'):
-
-        self.numcnh = numcnh
-        self.nome = nome
-        self.sobrenome = sobrenome
-        self.nascimento = nascimento
-
-    def get_motorista(self):
-        return self, self.numcnh, self.nome, self.sobrenome, self.nascimento
-
-    def set_motorista(self, nova_cnh, novo_nome, novo_sobrenome, nova_data):
-
-        self.numcnh = nova_cnh
-        self.nome = novo_nome
-        self.sobrenome = novo_sobrenome
-        self.nascimento = nova_data
-        novo_motorista = f"INSERT INTO [PEDRO_CANIZELA_MOTORISTA] VALUES ('{self.numcnh}', '{self.nome}', \
-                         '{self.sobrenome}', '{self.nascimento}' "
-        cursor.execute(novo_motorista)
-        cursor.comit()
 
 
 def menu_inicial():
@@ -259,7 +147,7 @@ def cadastrar_usuario():
     email = str(input('Digite seu email >>> '))
     bairro = str(input('Digite seu bairro >>> '))
     data_nascimento = str(input('Digite sua data de nascimrnto [YYYY-MM-DD] >>> '))
-    new_user.set_usuario(f'{nome}, {sobrenome}, {email}, {bairro}, {data_nascimento}')
+    new_user.set_usuario(nome, sobrenome, email, bairro, data_nascimento)
     print('Usuário cadastrado com sucesso, você será redirecionado para o '
           'menu inicial para decidir o que deseja realizar em seguida.')
     menu_inicial()
@@ -290,7 +178,7 @@ def cadastrar_cartao():
         print('Você digitou um valor inválido, preste mais atençao')
         cadastrar_cartao()
     data_emissao = str(input('Digite a data de emissao do seu cartao [YYYY-MM-DD] >>> '))
-    new_card.set_cartao(f'{id_cartao}, {id_usuario}, {creditos}, {tipo_cartao}, {data_emissao}')
+    new_card.set_cartao(id_cartao, id_usuario, creditos, tipo_cartao, data_emissao)
     print('Cartao cadastrado com sucesso, você será redirecionado para o '
           'menu inicial para decidir o que deseja realizar em seguida.')
     menu_inicial()
@@ -303,7 +191,7 @@ def cadastrar_onibus():
     modelo_bus = str(input('Digite o modelo do ônibus >>> '))
     ano_fabrica = str(input('Digite a data de fabricacao [YYYY-MM-DD] >>> '))
     id_motorista = int(input('Digite seu número de identificaçao >>> '))
-    new_bus.set_onibus(f'{num_placa}, {num_linha}, {modelo_bus}, {ano_fabrica}, {id_motorista}')
+    new_bus.set_onibus(num_placa, num_linha, modelo_bus, ano_fabrica, id_motorista)
     print('Ônibus cadastrado com sucesso, você será redirecionado para o '
           'menu inicial para decidir o que deseja realizar em seguida.')
     menu_inicial()
@@ -315,7 +203,7 @@ def cadastrar_motorista():
     nome = str(input('Digite seu nome >>> '))
     sobrenome = str(input('Digite seu sobrenome >>> '))
     data_nascimento = str(input('Digite sua data de nascimrnto [YYYY-MM-DD] >>> '))
-    new_driver.set_motorista(f'{cnh}, {nome}, {sobrenome}, {data_nascimento}')
+    new_driver.set_motorista(cnh, nome, sobrenome, data_nascimento)
     print('Motorista cadastrado com sucesso, você será redirecionado para o '
           'menu inicial para decidir o que deseja realizar em seguida.')
     menu_inicial()
